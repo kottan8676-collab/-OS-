3ddb5e3e41653ae28b8fcffa8817a8ab5465b061
kottan8676-collab <kottan8676@gmail.com>
2026-04-20 03:20:51 +0900

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,12 +2,12 @@ importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "あなたのapiKey",
-  authDomain: "あなたのauthDomain",
-  projectId: "あなたのprojectId",
-  storageBucket: "あなたのstorageBucket",
-  messagingSenderId: "あなたのmessagingSenderId",
-  appId: "あなたのappId"
+ apiKey: "AIzaSyDoMPkVXFb8yW57W9S5tX6Xk70AJmik_O4",
+  authDomain: "knowledgeos-19481.firebaseapp.com",
+  projectId: "knowledgeos-19481",
+  storageBucket: "knowledgeos-19481.firebasestorage.app",
+  messagingSenderId: "110904798378",
+  appId: "1:110904798378:web:6670dffaaa026c260734d3"
 });
 
 const messaging = firebase.messaging();
